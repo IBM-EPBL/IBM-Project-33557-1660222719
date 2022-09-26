72941073f76618526b406a8c05a7ba06ddb6aa97
Yogamalini S R <yogamalinirajendran@gmail.com>
2022-09-26 21:45:52 +0530

    Temp and Humidity Removed!

---
+++ Assignments/Assignment 1/Yogamalini S R/Circuit Code.py
@@ -0,0 +1,64 @@
+'''#single - line comments
+
+
+mlti
+line
+comments
+
+
+#variables
+a = 1
+print(a)
+print(type(a))
+
+b= 2.3
+print(b)
+print(type(b))
+
+c = 3+2j
+print(c)
+print(type(c))
+
+d = True
+print(d)
+print(type(d))
+
+i = "Yogamalini S R"
+print(i)
+print(type(i))
+print(len(i))
+print(i[2:9])
+print(i[::2])
+print(i[::-1])
+
+k = "Yogamalini S R"
+print(k[13:10:-1])
+print(k[-3:-6:-1])
+
+#strip
+m = "SARANYA SRI"
+print(m.strip())
+print(m.lstrip())
+print(m.rstrip())
+print(m.find("SRI"))
+print(m.replace("SRI","RS"))
+print(m.count('A'))
+'''
+'''#LIST
+
+l =[1,3,5,7,9,11]
+print(l, type(l))
+print(len(l))
+print(sum(l))
+print(l[2])
+l[3] =1000
+print(l)
+l.append(3)
+l.remove(9)
+l.reverse()
+l.sort(reverse = True)
+print(l)
+'''
+
+print("hello")
+
